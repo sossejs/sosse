38c81527a0167115ac3791305ac4a7dda2c2c0f1
v1rtl <pilll.PL22@gmail.com>
2020-07-16 17:52:25 +0300

    replace .m.js with .mjs to be working in Node ESM

--- scripts/module.js
+++ scripts/module.js
@@ -11,88 +11,88 @@ const {
   writeFile,
   scriptsDir,
   chmod,
-} = require("./lib");
+} = require('./lib')
 
 const updatePkg = async function (moduleName, pkg) {
   pkg.files.push(
     `${moduleName}/package.json`,
     `${moduleName}/lib`,
     `${moduleName}/dist`
-  );
-  pkg.files = Array.from(new Set(pkg.files));
+  )
+  pkg.files = Array.from(new Set(pkg.files))
 
-  pkg.jest.collectCoverageFrom.push(`${moduleName}/lib/**/*.{ts,tsx,js,jsx}`);
+  pkg.jest.collectCoverageFrom.push(`${moduleName}/lib/**/*.{ts,tsx,js,jsx}`)
   pkg.jest.collectCoverageFrom = Array.from(
     new Set(pkg.jest.collectCoverageFrom)
-  );
+  )
 
-  const exportsBase = `./${moduleName}`;
+  const exportsBase = `./${moduleName}`
   pkg.exports[exportsBase] = {
-    browser: `${exportsBase}/dist/main.m.js`,
+    browser: `${exportsBase}/dist/main.mjs`,
     umd: `${exportsBase}/dist/main.umd.js`,
-    import: `${exportsBase}/dist/main.m.js`,
+    import: `${exportsBase}/dist/main.mjs`,
     require: `${exportsBase}/dist/main.js`,
-  };
+  }
 
   // These entries should stay at the very end of the exports object
-  for (const entry of ["./package.json", "./"]) {
+  for (const entry of ['./package.json', './']) {
     if (!pkg.exports[entry]) {
-      continue;
+      continue
     }
 
-    const value = pkg.exports[entry];
-    delete pkg.exports[entry];
-    pkg.exports[entry] = value;
+    const value = pkg.exports[entry]
+    delete pkg.exports[entry]
+    pkg.exports[entry] = value
   }
 
-  await writeJson(pkgDir("package.json"), pkg, {
-    spaces: "  ",
-  });
-};
+  await writeJson(pkgDir('package.json'), pkg, {
+    spaces: '  ',
+  })
+}
 
 const updateTsconfig = async function (moduleName, pkg) {
-  const tsConfig = require("../tsconfig");
-  const pathAlias = `${pkg.name}/${moduleName}`;
+  const tsConfig = require('../tsconfig')
+  const pathAlias = `${pkg.name}/${moduleName}`
 
   if (tsConfig.compilerOptions.paths[pathAlias]) {
-    return;
+    return
   }
 
-  tsConfig.compilerOptions.paths[pathAlias] = [`../${moduleName}/lib/main.ts`];
-  const tsConfigPath = resolve(pkgDir("tsconfig.json"));
+  tsConfig.compilerOptions.paths[pathAlias] = [`../${moduleName}/lib/main.ts`]
+  const tsConfigPath = resolve(pkgDir('tsconfig.json'))
   await writeJson(tsConfigPath, tsConfig, {
-    spaces: "  ",
-  });
-};
+    spaces: '  ',
+  })
+}
 
 const createModulePkg = async function (moduleDir, moduleName, pkg) {
-  const modulePkg = resolve(moduleDir, "package.json");
+  const modulePkg = resolve(moduleDir, 'package.json')
   await writeJson(
     modulePkg,
     {
       name: `${pkg.name}-${moduleName}`,
       amdName: `${pkg.name}${upperFirst(moduleName)}`,
-      source: "lib/main.ts",
-      main: "dist/main.js",
-      module: "dist/main.m.js",
-      "umd:main": "dist/main.umd.js",
-      esmodule: "dist/main.modern.js",
-      types: "dist/main.d.ts",
+      source: 'lib/main.ts',
+      main: 'dist/main.js',
+      module: 'dist/main.mjs',
+      'umd:main': 'dist/main.umd.js',
+      esmodule: 'dist/main.modern.js',
+      types: 'dist/main.d.ts',
     },
     {
-      spaces: "  ",
+      spaces: '  ',
     }
-  );
-};
+  )
+}
 
 const createModuleLib = async function (moduleDir) {
-  const moduleLibDir = resolve(moduleDir, "lib");
-  await mkDir(moduleLibDir);
+  const moduleLibDir = resolve(moduleDir, 'lib')
+  await mkDir(moduleLibDir)
   await writeFile(
-    resolve(moduleLibDir, "main.ts"),
-    'export const hello = "world";' + "\n"
-  );
-};
+    resolve(moduleLibDir, 'main.ts'),
+    'export const hello = "world";' + '\n'
+  )
+}
 
 const createBuildScript = async function (moduleName) {
   const buildScript = `#!/usr/bin/env node
@@ -103,39 +103,39 @@ const { spawn, args, buildArgs, rmDir, pkgDir } = require("./lib");
   await rmDir(pkgDir("${moduleName}", "dist"), { recursive: true });
   spawn("npx", ["microbundle", "--cwd", "${moduleName}", "--raw", ...buildArgs(), ...args]);
 })();
-`;
+`
 
-  const scriptPath = scriptsDir(`build-${moduleName}.js`);
-  await writeFile(scriptPath, buildScript);
-  await chmod(scriptPath, "755");
-};
+  const scriptPath = scriptsDir(`build-${moduleName}.js`)
+  await writeFile(scriptPath, buildScript)
+  await chmod(scriptPath, '755')
+}
 
-(async () => {
+;(async () => {
   try {
-    const moduleName = (args[0] && args[0].trim()) || undefined;
+    const moduleName = (args[0] && args[0].trim()) || undefined
 
     if (!moduleName) {
-      console.error("Module name missing in arguments.");
-      return;
+      console.error('Module name missing in arguments.')
+      return
     }
 
-    const moduleDir = pkgDir(moduleName);
+    const moduleDir = pkgDir(moduleName)
 
     if (await exists(moduleDir)) {
-      console.error(`"${moduleDir}" already exists.`);
-      return;
+      console.error(`"${moduleDir}" already exists.`)
+      return
     }
 
-    await mkDir(moduleDir);
+    await mkDir(moduleDir)
 
-    const pkg = require("./../package");
+    const pkg = require('./../package')
 
-    await updatePkg(moduleName, pkg);
-    await updateTsconfig(moduleName, pkg);
-    await createModulePkg(moduleDir, moduleName, pkg);
-    await createModuleLib(moduleDir);
-    await createBuildScript(moduleName);
+    await updatePkg(moduleName, pkg)
+    await updateTsconfig(moduleName, pkg)
+    await createModulePkg(moduleDir, moduleName, pkg)
+    await createModuleLib(moduleDir)
+    await createBuildScript(moduleName)
   } catch (err) {
-    console.error(err);
+    console.error(err)
   }
-})();
+})()
