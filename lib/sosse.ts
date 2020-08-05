0c573873ca70d0a9cef713bdc38ba5797d7ec08c
Katja Lutz <mail@katjalutz.ch>
2020-08-05 13:57:28 +0200

    chore: remove obsolete counter from default main template

--- lib/sosse.ts
+++ lib/sosse.ts
@@ -90,16 +90,13 @@ export const sosse = async function ({
       await writeFile(
         serverSrcMain,
         `import { createServer, useHtml, jsx } from "sosse";
-import { css, htmlData } from "sosse/uni";
+import { css } from "sosse/uni";
 import { h } from "preact";
 
 export default () => {
   const html = useHtml();
   const server = createServer((req, res) => {
     if (req.url !== "/") return res.writeHead(500).end();
-
-    globalThis.counter++;
-    htmlData({ count: globalThis.counter });
 
     res.end(
       html(() => ({
