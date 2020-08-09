277fe970648a6830103458ce6869f3f6d6438592
Katja Lutz <mail@katjalutz.ch>
2020-08-09 23:35:11 +0200

    feat: export Asset type

--- lib/main.ts
+++ lib/main.ts
@@ -1,5 +1,5 @@
 export { hsr } from "./hsr";
 export { createServer, devSocket, withSosse } from "./server";
 export { useHtml, HtmlOptions, jsx } from "./html";
-export { setCtx, useCtx, unsetCtx, useAsset } from "./ctx";
+export { setCtx, useCtx, unsetCtx, useAsset, Asset } from "./ctx";
 export { sosse, SosseOptions } from "./sosse";
