3b3cb6ff5424d53c0b09f5cd71328b49a998b93e
Katja Lutz <mail@katjalutz.ch>
2020-09-16 12:46:03 +0200

    feat: export Ctx for consumption

--- lib/main.ts
+++ lib/main.ts
@@ -1,4 +1,4 @@
 export { hsr } from "./hsr";
 export { jsx } from "./html";
-export { setCtx, useCtx, unsetCtx, Asset, HtmlOptions } from "./ctx";
+export { setCtx, useCtx, unsetCtx, Asset, HtmlOptions, Ctx } from "./ctx";
 export { sosse, SosseOptions } from "./sosse";
