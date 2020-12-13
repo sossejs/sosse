21d0754d03771259256ed0ed35fcb52adaa6b7b7
Katja Lutz <mail@katjalutz.ch>
2020-12-13 13:49:14 +0100

    chore: add argv operands to start script

--- scripts/start.js
+++ scripts/start.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 
-const { spawn } = require("./lib");
+const { spawn, argvOperands } = require("./lib");
 
 // For dev: compile the cli first with NODE_ENV=development ./scripts/build-cli.js
-spawn("node", ["cli/dist/main.js", "start", "--cwd", "example"]);
+spawn("node", [
+  ...argvOperands,
+  "cli/dist/main.js",
+  "start",
+  "--cwd",
+  "example",
+]);
